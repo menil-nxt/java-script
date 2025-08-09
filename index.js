f26628c261c239184505e6513ad8adc3eba72682
Menil Jotaniya <menil.nxt@gmail.com>
2025-08-09 20:11:36 +0530

    operators

--- index.js
+++ index.js
@@ -62,3 +62,97 @@ function squre(number){
     return number*number;
 }
 console.log(squre(2));
+
+
+// Operators :-
+
+// Arithmetic operator.
+console.log('Arithmetic');
+
+
+let x = 3;
+let y = 4;
+
+console.log(x + y); // Adition operator 
+console.log(x - y); // Subtract operator 
+console.log(x * y); // Multipication operator 
+console.log(x / y); // Division operator 
+console.log(x % y); // Modulo (Reminders of division) operator 
+console.log(x ** y); // (x power y) Exponentiation operator 
+
+
+// Increment (++)
+console.log(++x); //value of x is increment by 1 first than it is seen on console.
+console.log(x++); // Only in this line value of x is increment by 1 but not show in console. 
+console.log(x); // for (x++) you want to console than print this line. 
+
+// Decrement (--)
+console.log(--y); // value of y is decrement by 1 first than it is seen on console.
+console.log(y--); // only in this line value of y is decriment by 1 but not show in console.
+console.log(y); // for (y--) you want to console thna print this line.
+
+
+// Assignment operator.
+console.log('Assignment');
+
+
+let a = 10; // here (=) is assignment operator value of x is 10.
+
+x = a + 1; // here line 97 or 98 both are equal.
+// a += 1;
+// a -= 1;
+// a *= 2;
+// a /= 2;
+console.log(x);
+
+// Comparisun Operator
+console.log('Comparisun');
+
+let b = 10
+
+// Relational Operator
+console.log(b > 0);
+console.log(b >= 10);
+console.log(b < 10);
+console.log(b <= 10);
+
+// Equality
+console.log('Equality');
+console.log(b === 10); // equal to check type or value both.
+console.log(b !== 10); // not equal to
+console.log(b == 10); // equal check only value
+
+// strict Equality (Type + value)
+console.log(1 === 1);
+console.log('1' === 1);
+
+// Lose Equality (value)
+console.log(1 == 1);
+console.log('1' == 1);
+console.log(true == 1);
+console.log(false == 1);
+
+// Ternary operator (Conditionnal)
+console.log('Ternary');
+/* If a customer has more than 100 points thay are a "Gold" customer ,
+ otherwise thay are a "silver" customer*/
+
+let points = 110;
+let check = points > 100 ? 'Gold' : 'Silver';
+console.log(check);
+
+// Logical operator
+console.log('Logical');
+
+// Logical AND (&&) return true if both operands are true 
+console.log(true && true);
+
+let highIncome = true ;
+let goodCreditScore = true ;
+let eligibleForLone = highIncome || goodCreditScore;
+console.log(eligibleForLone);
+
+
+// Not (!)
+let AplicibleForLone = !eligibleForLone;
+console.log(AplicibleForLone)
