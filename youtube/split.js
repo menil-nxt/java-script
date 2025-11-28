1ad035678397cd8e55078376243cadccc18ae24c
Menil Jotaniya <menil.nxt@gmail.com>
2025-11-28 18:04:37 +0530

    split function

---
+++ youtube/split.js
@@ -0,0 +1,15 @@
+// Split function
+
+// convert string into array diffrent part
+
+// syntax :-  string.split(separator, limit)
+
+let str = "my name is menil jotaniya";
+let result = str.split(" "); // (" ") -> seprator
+console.log(result);
+
+// with limit
+let result2 = str.split(" ", 2); // (" ") -> seprator (,2) -> limit
+// give you array of element to 0 and 1 index ;
+
+console.log(result2);
